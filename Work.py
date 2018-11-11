5a7179e86a933395443c1252e1993ef154553777
Phantom <ncwick@student.rtc.edu>
2018-11-11 00:03:48 -0800

    Checked for errors, commented out WIP section, and re checked for errors, seems to be running smoothly.

--- Work.py
+++ Work.py
@@ -15,7 +15,7 @@ import time
 
 conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='Computer_Parts')
 c = conn.cursor()
-c.execute("CREATE TABLE FIXTEST(id INTEGER PRIMARY KEY, Date TEXT, Class TEXT, Name TEXT, Model INTEGER, "
+c.execute("CREATE TABLE Computer_Parts(id INTEGER PRIMARY KEY, Date TEXT, Class TEXT, Name TEXT, Model INTEGER, "
           "Manufacturer TEXT, Quantity INTEGER, Price INTEGER, Site TEXT)")
 
 # Creates test entry to ensure things are going as they should be.
@@ -35,18 +35,18 @@ am = "Amazon"; bb = "Best_Buy"; f = "Frys"; ne = "New_Egg"
 # Runs with exit code 0, no errors, and now creates and writes to the db/table
 # Work in progress below
 
-counter = 0
-query = "#INSERT URL(S) HERE#"
-jsonpage = 0
-try:
-    contents = urllib.request.urlopen(query)
-    response = contents.read()
-    jsonpage = json.loads(response)
-except:
-    pass
-    time.sleep(1)
-data = json.dumps(jsonpage['data'])
-for i in data:
-    conn.c(
-        "INSERT INTO Computer_Parts(#VAR STUFF HERE#+#TABLE VALUES/DATA#)"
-    )
+#counter = 0
+#query = "#INSERT URL(S) HERE#"
+#jsonpage = 0
+#try:
+#    contents = urllib.request.urlopen(query)
+#    response = contents.read()
+#    jsonpage = json.loads(response)
+#except:
+#    pass
+#    time.sleep(1)
+#data = json.dumps(jsonpage['data'])
+#for i in data:
+#    conn.c(
+#        "INSERT INTO Computer_Parts(#VAR STUFF HERE#+#TABLE VALUES/DATA#)"
+#    )
