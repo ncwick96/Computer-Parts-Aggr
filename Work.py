12ebd2e9c3570f55f57a75a4d84f0a71a4cd54dc
Phantom <ncwick@student.rtc.edu>
2018-11-19 12:28:16 -0800

    Fixed Date, changed column orientation, etc.
    Fixed Stuff

--- Work.py
+++ Work.py
@@ -17,12 +17,12 @@ import time
 
 conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='Computer_Parts')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS Computer_Parts(id INTEGER PRIMARY KEY AUTO_INCREMENT, Date TEXT, "
-          "Class TEXT, Name TEXT, Model INTEGER,Manufacturer TEXT, Quantity INTEGER, Price INTEGER, Site TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS Computer_Parts(id INTEGER PRIMARY KEY AUTO_INCREMENT, Date DATE, "
+          "Name TEXT, Class TEXT, Quantity INTEGER, Model INTEGER,Manufacturer TEXT, Price INTEGER, Site TEXT)")
 
 # Creates test entry to ensure things are going as they should be.
-c.execute("INSERT INTO Computer_Parts(Date, Class, Name, Model, Manufacturer, Quantity, Price, Site) "
-          "Values('11/9/18','RAM','Corsair 64gb RAM 3200','224873','Corsair','64','550.98','Amazon')")
+c.execute("INSERT INTO Computer_Parts(Date, Name, Class, Quantity, Model, Manufacturer, Price, Site) "
+          "Values(NOW(),'Corsair 64gb RAM 3200','RAM','64','224873','Corsair','550.98','Amazon')")
 c.close()
 
 
@@ -37,18 +37,18 @@ eb = "Ebay"; me = "Mouser_Electronics"; ppp = "Pc_Part_Picker"; ne = "New_Egg"
 # Runs with exit code 0, no errors, and now creates and writes to the db/table
 # Work in progress below
 
-counter = 0
-query = "https://www.newegg.com/"
-jsonpage = 0
-try:
-    contents = urllib.request.urlopen(query)
-    response = contents.read()
-    jsonpage = json.loads(response)
-except:
-    pass
-    time.sleep(1)
-data = json.dumps(jsonpage(c))
-for i in data:
-    conn.c(
-        "INSERT INTO Computer_Parts(#VAR STUFF HERE#+#TABLE VALUES/DATA#)"
-    )
+#counter = 0
+#query = "https://www.newegg.com/"
+#jsonpage = 0
+#try:
+#    contents = urllib.request.urlopen(query)
+#    response = contents.read()
+#    jsonpage = json.loads(response)
+#except:
+#    pass
+#    time.sleep(1)
+#data = json.dumps(jsonpage(c))
+#for i in data:
+#    conn.c(
+#        "INSERT INTO Computer_Parts(#VAR STUFF HERE#+#TABLE VALUES/DATA#)"
+#    )
