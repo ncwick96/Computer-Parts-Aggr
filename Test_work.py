39b3f4c25bf3bfa608e80a6ebd619cd8dfcb766a
Phantom <ncwick@student.rtc.edu>
2018-11-26 09:20:58 -0800

    Changes in test_work
    Changed out a lot of stuff, added and fixed variables, made file path permanent, so that it will always work for everyone.

--- Test_work.py
+++ Test_work.py
@@ -2,38 +2,43 @@ import requests
 import openapi
 
 # Make a get request to get the pc part picker api api.
-response = requests.get("http://github.com/ncwick96/Computer-Parts-Aggr/blob/master/pcpartpicker.py/")
+req = requests.get("https://www.mouser.com/")
 
 # Print the status code of the response.
-print(response.status_code)
+result = "print(response.status_code)"
 
 # Set up the parameters we want to pass to the API.
-parameters = {"RAM: 16gb","Heat_Sink"}
+params = {"16gb","Corsair"}
 
 # Make a get request with the parameters.
-#response = requests.get("http://github.com/ncwick96/Computer-Parts-Aggr/blob/master/pcpartpicker.py/", params=parameters)
+# response = requests.get("http://github.com/ncwick96/Computer-Parts-Aggr/blob/master/pcpartpicker.py/"
+
 
 # Print the content of the response (the data the server returned)
-print(response.content)
+rescon = "print(response.content)"
 
 
 # Assumes that the key is plaintext
-# Returns the API key stored in the file found at relative path C:\Users\Parker.DRAGON.001\Documents\CNA\CNA 330\Computer-Parts-Aggr\test_APIkey.txt
+# Returns the API key stored in the file found at relative path ".\APIS\Mouser Electronics.txt"
 def get_api_key_from_file():
-    with open(r'''C:\Users\Parker.DRAGON.001\Documents\CNA\CNA 330\Computer-Parts-Aggr\test_APIkey.txt''') as f:
+    with open(r'''.\APIS\Mouser Electronics.txt''') as f:
         return f.read()
 
 
-#reads the pcpartpicker.py file in our github repository
-    uri += region + "github.com/ncwick96/Computer-Parts-Aggr/blob/master/pcpartpicker.py/"
-    uri += achievement_id + "?locale=" + locale + "&apikey=" + apikey
-    verboseprint(url)
-    return uri
+# reads the pcpartpicker.py file in our github repository
+#   url += region + "github.com/ncwick96/Computer-Parts-Aggr/blob/master/pcpartpicker.py/"
+#   url += achievement_id + "?locale=" + locale + "&apikey=" + apikey
+#   verboseprint(url)
+#   return url
 
 
+    url += region + "https://mouser.com"
+    return url
+    result
+    rescon
 
-  #  uri = build_achievement_request(request_args)
-    response = requests.get(uri)
+    url = build_achievement_request(request_args)
+    response = requests.get(url)
     print(response.json())
     print(url)
 
